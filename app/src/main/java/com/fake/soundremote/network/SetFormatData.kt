09c40f4fbe4733a7ae6131148679185eae4cc87e
ashipo <24320267+ashipo@users.noreply.github.com>
2023-10-31 23:08:18 +0300

    Add PacketData subclass SetFormatData for set format packet

--- app/src/main/java/com/fake/soundremote/network/SetFormatData.kt
+++ app/src/main/java/com/fake/soundremote/network/SetFormatData.kt
@@ -8,7 +8,7 @@ unsigned 8bit    compression
 */
 private const val SIZE = 1
 
-data class ConnectData(@Net.Compression val compression: Int) : PacketData {
+data class SetFormatData(@Net.Compression val compression: Int) : PacketData {
     override fun writeToBuffer(dest: ByteBuffer) {
         require(dest.remaining() >= SIZE)
         dest.put(compression.toByte())
