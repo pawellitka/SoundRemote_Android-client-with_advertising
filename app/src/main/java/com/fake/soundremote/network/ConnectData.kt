19b7ae738a7f1abca6d0c4462bcefdd169b8ecc7
ashipo <24320267+ashipo@users.noreply.github.com>
2023-10-31 22:52:49 +0300

    Add PacketData subclass ConnectData for connect packet

---
+++ app/src/main/java/com/fake/soundremote/network/ConnectData.kt
@@ -0,0 +1,19 @@
+package com.fake.soundremote.network
+
+import com.fake.soundremote.util.Net
+import java.nio.ByteBuffer
+
+/*
+unsigned 8bit    compression
+*/
+private const val SIZE = 1
+
+data class ConnectData(@Net.Compression val compression: Int) : PacketData {
+    override fun writeToBuffer(dest: ByteBuffer) {
+        require(dest.remaining() >= SIZE)
+        dest.put(compression.toByte())
+    }
+
+    override val size: Int
+        get() = SIZE
+}
