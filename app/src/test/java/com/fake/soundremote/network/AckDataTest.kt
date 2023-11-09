d32ca72de48e9d33e5f1328169780d52b0944713
ashipo <24320267+ashipo@users.noreply.github.com>
2023-11-09 22:00:21 +0300

    Add test for AckData.read method

---
+++ app/src/test/java/com/fake/soundremote/network/AckDataTest.kt
@@ -0,0 +1,30 @@
+package com.fake.soundremote.network
+
+import com.fake.soundremote.util.Net
+import com.fake.soundremote.util.Net.putUShort
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("AckData")
+internal class AckDataTest {
+    @DisplayName("Reads correctly")
+    @Test
+    fun ackData_ReadsCorrectly() {
+        val requestId: UShort = 0xFAAFu
+        val customData = 0xFA123456.toInt()
+        val expectedCustomData = Net.createPacketBuffer(AckData.CUSTOM_DATA_SIZE)
+            .putInt(customData)
+        expectedCustomData.rewind()
+        val expected = AckData(requestId, expectedCustomData)
+        val packet = Net.createPacketBuffer(20)
+            .putUShort(requestId)
+            .putInt(customData)
+        packet.rewind()
+
+        val actual = AckData.read(packet)
+
+        Assertions.assertNotNull(actual)
+        Assertions.assertEquals(expected, actual!!)
+    }
+}
