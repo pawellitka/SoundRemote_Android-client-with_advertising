663fdcac025eb803151f369a9c58fd01139bdea7
ashipo <24320267+ashipo@users.noreply.github.com>
2023-11-17 22:24:05 +0300

    Add NavigationTest

---
+++ app/src/androidTest/java/com/fake/soundremote/NavigationTest.kt
@@ -0,0 +1,25 @@
+package com.fake.soundremote
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class NavigationTest {
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun firstScreen_isHomeScreen() {
+        composeTestRule
+            .onNodeWithText(composeTestRule.activity.getString(R.string.home_title))
+            .assertIsDisplayed()
+    }
+}
