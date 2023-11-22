da1b7d1198407ccf7386da7fee3f3643429275a6
ashipo <24320267+ashipo@users.noreply.github.com>
2023-11-22 19:48:24 +0300

    Rename KeystrokeRepository to UserKeystrokeRepository

--- app/src/main/java/com/fake/soundremote/data/UserKeystrokeRepository.kt
+++ app/src/main/java/com/fake/soundremote/data/UserKeystrokeRepository.kt
@@ -10,54 +10,55 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class KeystrokeRepository(
+class UserKeystrokeRepository(
     private val keystrokeDao: KeystrokeDao,
     private val keystrokeOrderDao: KeystrokeOrderDao,
     private val dispatcher: CoroutineDispatcher,
-) {
+) : KeystrokeRepository {
     @Inject
     constructor(keystrokeDao: KeystrokeDao, keystrokeOrderDao: KeystrokeOrderDao) :
             this(keystrokeDao, keystrokeOrderDao, Dispatchers.IO)
 
-    suspend fun getById(id: Int): Keystroke? = withContext(dispatcher) {
+    override suspend fun getById(id: Int): Keystroke? = withContext(dispatcher) {
         keystrokeDao.getById(id)
     }
 
-    suspend fun insert(keystroke: Keystroke) = withContext(dispatcher) {
+    override suspend fun insert(keystroke: Keystroke) = withContext(dispatcher) {
         keystrokeDao.insert(keystroke)
     }
 
-    suspend fun update(keystroke: Keystroke) = withContext(dispatcher) {
+    override suspend fun update(keystroke: Keystroke) = withContext(dispatcher) {
         keystrokeDao.update(keystroke)
     }
 
-    suspend fun delete(keystroke: Keystroke) = withContext(dispatcher) {
+    override suspend fun delete(keystroke: Keystroke) = withContext(dispatcher) {
         keystrokeDao.delete(keystroke)
     }
 
-    suspend fun deleteById(id: Int) = withContext(dispatcher) {
+    override suspend fun deleteById(id: Int) = withContext(dispatcher) {
         keystrokeDao.deleteById(id)
     }
 
-    suspend fun changeFavoured(id: Int, favoured: Boolean) = withContext(dispatcher) {
+    override suspend fun changeFavoured(id: Int, favoured: Boolean) = withContext(dispatcher) {
         keystrokeDao.changeFavoured(id, favoured)
     }
 
-    suspend fun getOrderedOneshot(): List<Keystroke> = withContext(dispatcher) {
+    override suspend fun getOrderedOneshot(): List<Keystroke> = withContext(dispatcher) {
         keystrokeDao.getOrderedOneshot()
     }
 
-    fun getFavouredOrdered(favoured: Boolean): Flow<List<Keystroke>> =
+    override fun getFavouredOrdered(favoured: Boolean): Flow<List<Keystroke>> =
         keystrokeDao.getFavouredOrdered(favoured)
 
-    fun getAllOrdered(): Flow<List<Keystroke>> =
+    override fun getAllOrdered(): Flow<List<Keystroke>> =
         keystrokeDao.getAllOrdered()
 
-    suspend fun getAllOrdersOneshot(): List<KeystrokeOrder> = withContext(dispatcher) {
+    override suspend fun getAllOrdersOneshot(): List<KeystrokeOrder> = withContext(dispatcher) {
         keystrokeOrderDao.getAllOneshot()
     }
 
-    suspend fun updateOrders(keystrokeOrders: List<KeystrokeOrder>) = withContext(dispatcher) {
-        keystrokeOrderDao.updateAll(keystrokeOrders)
-    }
+    override suspend fun updateOrders(keystrokeOrders: List<KeystrokeOrder>) =
+        withContext(dispatcher) {
+            keystrokeOrderDao.updateAll(keystrokeOrders)
+        }
 }
