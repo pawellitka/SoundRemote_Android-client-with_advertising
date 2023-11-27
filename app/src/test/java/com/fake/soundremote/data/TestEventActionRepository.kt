5b36d1ace7e003ffde3187e64639e67914391648
ashipo <24320267+ashipo@users.noreply.github.com>
2023-11-27 12:26:19 +0300

    Refactor TestEventActionRepository
    Replaced inner data backing MutableStateFlow with MutableSharedFlow.

--- app/src/test/java/com/fake/soundremote/data/TestEventActionRepository.kt
+++ app/src/test/java/com/fake/soundremote/data/TestEventActionRepository.kt
@@ -1,47 +1,52 @@
 package com.fake.soundremote.data
 
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.MutableSharedFlow
 
 class TestEventActionRepository : EventActionRepository {
-    private val _eventActionsFlow = MutableStateFlow<List<EventAction>>(emptyList())
+    private val eventActionsFlow: MutableSharedFlow<List<EventAction>> = MutableSharedFlow(
+        replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    private val currentEventActions: List<EventAction>
+        get() = eventActionsFlow.replayCache.firstOrNull() ?: emptyList()
 
     override suspend fun getById(id: Int): EventAction? {
-        return _eventActionsFlow.value.find { it.eventId == id }?.copy()
+        return currentEventActions.find { it.eventId == id }?.copy()
     }
 
     override suspend fun insert(eventAction: EventAction) {
-        _eventActionsFlow.update { it + eventAction }
+        val existing = currentEventActions.find { it.eventId == eventAction.eventId }
+        if (existing == null) {
+            eventActionsFlow.tryEmit(currentEventActions + eventAction)
+        } else {
+            existing.keystrokeId = eventAction.keystrokeId
+            eventActionsFlow.tryEmit(currentEventActions)
+        }
     }
 
     override suspend fun update(eventAction: EventAction): Int {
-        _eventActionsFlow.update { events ->
-            val indexToUpdate = events.indexOfFirst { it.eventId == eventAction.eventId }
-            if (indexToUpdate == -1) return 0
-            events.toMutableList().apply {
-                set(indexToUpdate, eventAction)
-            }
-        }
+        val toUpdate = currentEventActions.find { it.eventId == eventAction.eventId } ?: return 0
+        toUpdate.keystrokeId = eventAction.keystrokeId
+        eventActionsFlow.tryEmit(currentEventActions)
         return 1
     }
 
     override suspend fun deleteById(id: Int) {
-        _eventActionsFlow.update { events ->
-            events.toMutableList().apply {
-                removeIf { it.eventId == id }
-            }
-        }
+        val alteredList = currentEventActions.toMutableList()
+        // If nothing to remove, do not emit
+        if (!alteredList.removeIf { it.eventId == id }) return
+        eventActionsFlow.tryEmit(alteredList)
     }
 
     override suspend fun getKeystrokeByEventId(id: Int): Keystroke? {
         TODO("Not yet implemented")
     }
 
-    override fun getAll(): Flow<List<EventAction>> = _eventActionsFlow
+    override fun getAll(): Flow<List<EventAction>> = eventActionsFlow
 
     // Test methods
     fun setEventActions(eventActions: List<EventAction>) {
-        _eventActionsFlow.value = eventActions
+        eventActionsFlow.tryEmit(eventActions)
     }
 }
