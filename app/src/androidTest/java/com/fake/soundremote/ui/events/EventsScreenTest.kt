1c871533b0024c30ee38ed89fa1e8568e6f01ce7
ashipo <24320267+ashipo@users.noreply.github.com>
2023-11-27 21:42:59 +0300

    Add tests in EventScreenTest
    Test permission info button display status

--- app/src/androidTest/java/com/fake/soundremote/ui/events/EventsScreenTest.kt
+++ app/src/androidTest/java/com/fake/soundremote/ui/events/EventsScreenTest.kt
@@ -11,6 +11,7 @@ import com.fake.soundremote.R
 import com.fake.soundremote.data.Event
 import com.fake.soundremote.stringResource
 import com.fake.soundremote.ui.theme.SoundRemoteTheme
+import com.fake.soundremote.util.AppPermission
 import org.junit.Rule
 import org.junit.Test
 
@@ -19,6 +20,7 @@ internal class EventsScreenTest {
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     private val navigateUp by composeTestRule.stringResource(R.string.navigate_up)
+    private val permissionInfo by composeTestRule.stringResource(R.string.permission_show_info_caption)
 
     // Events screen should contain navigate up arrow
     @Test
@@ -57,6 +59,38 @@ internal class EventsScreenTest {
         }
 
         composeTestRule.onNodeWithText(keystrokeName).assertIsDisplayed()
+    }
+
+    // Event without a permission doesn't show permission info button
+    @Test
+    fun permissionInfoButton_eventWithoutPermission_doesNotExist() {
+        composeTestRule.setContent {
+            val events = listOf(
+                EventUIState(1, Event.CALL_BEGIN.nameStringId, null, 1, "Keystroke name"),
+            )
+            CreateEventsScreen(eventsUIState = EventsUIState(events))
+        }
+
+        composeTestRule.onNodeWithContentDescription(permissionInfo).assertDoesNotExist()
+    }
+
+    // Event with a permission shows permission info button
+    @Test
+    fun permissionInfoButton_eventWithPermission_isDisplayed() {
+        composeTestRule.setContent {
+            val events = listOf(
+                EventUIState(
+                    1,
+                    Event.CALL_BEGIN.nameStringId,
+                    AppPermission.Phone,
+                    1,
+                    "Keystroke name"
+                ),
+            )
+            CreateEventsScreen(eventsUIState = EventsUIState(events))
+        }
+
+        composeTestRule.onNodeWithContentDescription(permissionInfo).assertIsDisplayed()
     }
 
     @Composable
