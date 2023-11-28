51614e1a88d1da95cea025a0e5b6ec2bc3ea6a48
ashipo <24320267+ashipo@users.noreply.github.com>
2023-11-28 15:30:12 +0300

    Update app version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.fake.soundremote"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.2.1"
+        versionCode = 2
+        versionName = "0.3.0"
         testInstrumentationRunner = "com.fake.soundremote.CustomTestRunner"
     }
     buildTypes {
