6b092798cbace566fb2db881d819baeb7e020a29
ashipo <24320267+ashipo@users.noreply.github.com>
2023-12-01 18:54:19 +0300

    Improve accessibility in KeystrokeListScreen
    Added content description for favoured switch.

--- app/src/main/java/com/fake/soundremote/ui/keystrokelist/KeystrokeListScreen.kt
+++ app/src/main/java/com/fake/soundremote/ui/keystrokelist/KeystrokeListScreen.kt
@@ -44,6 +44,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
@@ -215,17 +217,20 @@ private fun KeystrokeItem(
         Row(
             verticalAlignment = Alignment.CenterVertically,
         ) {
+            val switchDescription = stringResource(R.string.keystroke_favourite_toggle_description)
             Switch(
                 checked = favoured,
                 onCheckedChange = onChangeFavoured,
-                modifier = Modifier.padding(horizontal = 8.dp)
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .semantics { contentDescription = switchDescription }
             )
             Column(
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier.weight(1f)
             ) {
-                ListItemHeadline(text = name)
-                ListItemSupport(text = description)
+                ListItemHeadline(name)
+                ListItemSupport(description)
             }
             Icon(
                 Icons.Default.Menu,
