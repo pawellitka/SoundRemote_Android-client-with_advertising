4d1708be6ed6028bc89275398617fcb8904590f3
ashipo <24320267+ashipo@users.noreply.github.com>
2023-12-09 20:17:35 +0300

    Fix system bars on older APIs

--- app/src/main/java/com/fake/soundremote/MainActivity.kt
+++ app/src/main/java/com/fake/soundremote/MainActivity.kt
@@ -8,6 +8,7 @@ import android.media.AudioManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.content.ContextCompat
 import com.fake.soundremote.service.MainService
 import com.fake.soundremote.ui.SoundRemoteApp
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             SoundRemoteTheme {
                 SoundRemoteApp()
