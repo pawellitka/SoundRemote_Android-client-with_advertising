pawellitka/SoundRemote_Android-client-with_advertising

commit 4d7bcce315458ecacc44e9c1c3413c79aa676c51

Add arguments check to KeystrokeSelectDialog
Should throw IllegalArgumentException if initialKeystrokeId is not null or
equal to one of the KeystrokeUIState in the items argument.

app/src/main/java/com/fake/soundremote/ui/components/KeystrokeSelectDialog.kt
@@ -65,6 +65,7 @@ internal fun KeystrokeSelectDialog(
     onConfirm: (Int?) -> Unit,
     onDismiss: () -> Unit,
 ) {
+    require(initialKeystrokeId == null || items.find { it.id == initialKeystrokeId } != null)
     var selectedKeystrokeId by rememberSaveable { mutableStateOf(initialKeystrokeId) }
 
     AlertDialog(

commit 55b4c0bde23be291f849ae28807f6c100b5e05fa

Fix events screen bug introduced in 4d7bcce

app/src/main/java/com/fake/soundremote/ui/components/KeystrokeSelectDialog.kt
@@ -65,7 +65,6 @@ internal fun KeystrokeSelectDialog(
     onConfirm: (Int?) -> Unit,
     onDismiss: () -> Unit,
 ) {
-    require(initialKeystrokeId == null || items.find { it.id == initialKeystrokeId } != null)
     var selectedKeystrokeId by rememberSaveable { mutableStateOf(initialKeystrokeId) }
 
     AlertDialog(