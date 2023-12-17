0bf05252bd5b6aade167fc294a4573d4996276d7
ashipo <24320267+ashipo@users.noreply.github.com>
2023-12-18 00:17:45 +0300

    Bump version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.fake.soundremote"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.3.0"
+        versionCode = 3
+        versionName = "0.3.1"
         testInstrumentationRunner = "com.fake.soundremote.CustomTestRunner"
     }
     buildTypes {
