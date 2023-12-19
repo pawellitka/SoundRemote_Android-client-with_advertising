3e18b1ca1477ef817707423ab2328768f180b518
ashipo <24320267+ashipo@users.noreply.github.com>
2023-12-19 15:28:48 +0300

    Add method to ease creating Keystroke in unit tests

--- app/src/test/java/com/fake/soundremote/Helpers.kt
+++ app/src/test/java/com/fake/soundremote/Helpers.kt
@@ -1,7 +1,23 @@
 package com.fake.soundremote
 
 import com.fake.soundremote.data.Keystroke
+import com.fake.soundremote.util.KeyCode
 
 fun createKeystrokeWithMods(mods: Int?): Keystroke {
-    return Keystroke(0x2D, "Name", mods, false)
+    return Keystroke(KeyCode(0x2D), "Name", mods, false)
+}
+
+/**
+ * Creates a [Keystroke] with the specified parameters. Other parameters will have arbitrary default
+ * values.
+ */
+fun getKeystroke(
+    id: Int = 0,
+    keyCode: KeyCode = KeyCode(0x42),
+    mods: Int = 0,
+    name: String = "Keystroke name",
+    favoured: Boolean = false,
+    order: Int = 0,
+): Keystroke {
+    return Keystroke(id, keyCode, mods, name, favoured, order)
 }
