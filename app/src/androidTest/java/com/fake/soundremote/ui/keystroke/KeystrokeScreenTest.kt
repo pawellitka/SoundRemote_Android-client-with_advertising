8649f77a66990da9fa6b4b28e1469f949ed15a96
ashipo <24320267+ashipo@users.noreply.github.com>
2023-12-23 22:42:33 +0300

    Add tests for mods checkboxes in KeystrokeScreenTest

--- app/src/androidTest/java/com/fake/soundremote/ui/keystroke/KeystrokeScreenTest.kt
+++ app/src/androidTest/java/com/fake/soundremote/ui/keystroke/KeystrokeScreenTest.kt
@@ -7,10 +7,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.fake.soundremote.R
 import com.fake.soundremote.stringResource
 import com.fake.soundremote.ui.theme.SoundRemoteTheme
 import com.fake.soundremote.util.KeyCode
+import com.fake.soundremote.util.ModKey
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -28,6 +32,75 @@ internal class KeystrokeScreenTest {
         }
 
         composeTestRule.onNodeWithContentDescription(navigateUp).assertIsDisplayed()
+    }
+
+    // Mods
+    @Test
+    fun winMod_click_changesWinMod() {
+        val initial = true
+        val expected = !initial
+        var actual: Boolean? = null
+        composeTestRule.setContent {
+            CreateKeystrokeScreen(
+                state = KeystrokeScreenUIState(win = initial),
+                onWinChange = { actual = it },
+            )
+        }
+
+        composeTestRule.onNodeWithText(ModKey.WIN.label).performClick()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun ctrlMod_click_changesCtrlMod() {
+        val initial = true
+        val expected = !initial
+        var actual: Boolean? = null
+        composeTestRule.setContent {
+            CreateKeystrokeScreen(
+                state = KeystrokeScreenUIState(ctrl = initial),
+                onCtrlChange = { actual = it },
+            )
+        }
+
+        composeTestRule.onNodeWithText(ModKey.CTRL.label).performClick()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shiftMod_click_changesShiftMod() {
+        val initial = true
+        val expected = !initial
+        var actual: Boolean? = null
+        composeTestRule.setContent {
+            CreateKeystrokeScreen(
+                state = KeystrokeScreenUIState(shift = initial),
+                onShiftChange = { actual = it },
+            )
+        }
+
+        composeTestRule.onNodeWithText(ModKey.SHIFT.label).performClick()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun altMod_click_changesAltMod() {
+        val initial = true
+        val expected = !initial
+        var actual: Boolean? = null
+        composeTestRule.setContent {
+            CreateKeystrokeScreen(
+                state = KeystrokeScreenUIState(alt = initial),
+                onAltChange = { actual = it },
+            )
+        }
+
+        composeTestRule.onNodeWithText(ModKey.ALT.label).performClick()
+
+        assertEquals(expected, actual)
     }
 
     @Suppress("TestFunctionName")
