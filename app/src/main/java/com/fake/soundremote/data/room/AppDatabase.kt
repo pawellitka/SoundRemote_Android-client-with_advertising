de79394e314f05cb647d1f0d7b49d6b2e3bd3cb3
ashipo <24320267+ashipo@users.noreply.github.com>
2023-12-28 21:44:29 +0300

    Add db trigger to delete event action on keystroke delete
    Added SQL query to create db trigger that will delete event actions tied to a
    keystroke when that keystroke is deleted.

--- app/src/main/java/com/fake/soundremote/data/room/AppDatabase.kt
+++ app/src/main/java/com/fake/soundremote/data/room/AppDatabase.kt
@@ -2,6 +2,7 @@ package com.fake.soundremote.data.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.fake.soundremote.data.ActionType
 import com.fake.soundremote.data.EventAction
 import com.fake.soundremote.data.Keystroke
 
@@ -17,3 +18,14 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun keystrokeDao(): KeystrokeDao
     abstract fun eventActionDao(): EventActionDao
 }
+
+// When a keystroke is deleted also delete all event actions with that keystroke
+val DELETE_EVENT_ACTION_ON_KEYSTROKE_DELETE = """
+    CREATE TRIGGER IF NOT EXISTS delete_event_action_on_keystroke_delete
+    AFTER DELETE ON ${Keystroke.TABLE_NAME}
+    BEGIN
+        DELETE FROM ${EventAction.TABLE_NAME}
+        WHERE ${EventAction.COLUMN_ACTION_TYPE} = ${ActionType.KEYSTROKE.id}
+        AND ${EventAction.COLUMN_ACTION_ID} = OLD.${Keystroke.COLUMN_ID};
+    END
+    """.trimIndent()
