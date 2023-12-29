57d51b64c6e034e67c3e397fad764f033d148596
ashipo <24320267+ashipo@users.noreply.github.com>
2023-12-29 16:51:05 +0300

    Embed Action into EventAction entity

--- app/src/main/java/com/fake/soundremote/data/EventAction.kt
+++ app/src/main/java/com/fake/soundremote/data/EventAction.kt
@@ -2,10 +2,10 @@ package com.fake.soundremote.data
 
 import android.provider.BaseColumns
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.ForeignKey
-import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
+import com.fake.soundremote.data.room.Action
 
 @Entity(
     tableName = EventAction.TABLE_NAME,
@@ -14,17 +14,11 @@ data class EventAction internal constructor(
     @ColumnInfo(name = COLUMN_ID) @PrimaryKey
     var eventId: Int,
 
-    @ColumnInfo(name = COLUMN_ACTION_TYPE)
-    var actionType: Int,
-
-    @ColumnInfo(name = COLUMN_ACTION_ID)
-    var actionId: Int,
+    @Embedded val action: Action
 ) {
     companion object {
         const val TABLE_NAME = "event_action"
         const val COLUMN_ID = BaseColumns._ID
-        const val COLUMN_ACTION_TYPE = "action_type"
-        const val COLUMN_ACTION_ID = "action_id"
         const val COLUMN_KEYSTROKE_ID = "keystroke_id"
     }
 }
