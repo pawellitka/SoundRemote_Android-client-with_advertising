4b9cdb647274dd2d4fc3278181955f0b2861533d
ashipo <24320267+ashipo@users.noreply.github.com>
2024-01-07 21:38:57 +0300

    Add applicableActionTypes param to Event enum
    Represents types of action that can be assigned to that Event

--- app/src/main/java/com/fake/soundremote/data/Event.kt
+++ app/src/main/java/com/fake/soundremote/data/Event.kt
@@ -8,11 +8,24 @@ internal enum class Event(
     val id: Int,
     @StringRes
     val nameStringId: Int,
+    val applicableActionTypes: Set<ActionType>,
     val requiredPermission: AppPermission?,
     // Min SDK version for permission to be requested for the event.
     // If == null permission will always be requested.
     val permissionMinSdk: Int?,
 ) {
-    CALL_BEGIN(1, R.string.event_name_call_start, AppPermission.Phone, 31),
-    CALL_END(2, R.string.event_name_call_end, AppPermission.Phone, 31);
+    CALL_BEGIN(
+        1,
+        R.string.event_name_call_start,
+        setOf(ActionType.KEYSTROKE),
+        AppPermission.Phone,
+        31
+    ),
+    CALL_END(
+        2,
+        R.string.event_name_call_end,
+        setOf(ActionType.KEYSTROKE),
+        AppPermission.Phone,
+        31
+    );
 }
