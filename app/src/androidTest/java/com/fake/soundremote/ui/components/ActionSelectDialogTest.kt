5b5ff445f68bbbe039f8f789fdb46b422214d763
ashipo <24320267+ashipo@users.noreply.github.com>
2024-01-12 19:28:09 +0300

    Create ActionSelectDialogTest

---
+++ app/src/androidTest/java/com/fake/soundremote/ui/components/ActionSelectDialogTest.kt
@@ -0,0 +1,117 @@
+package com.fake.soundremote.ui.components
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.fake.soundremote.R
+import com.fake.soundremote.data.Action
+import com.fake.soundremote.data.ActionType
+import com.fake.soundremote.data.AppAction
+import com.fake.soundremote.stringResource
+import com.fake.soundremote.ui.theme.SoundRemoteTheme
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import kotlin.enums.enumEntries
+
+class ActionSelectDialogTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private val cancel by composeTestRule.stringResource(R.string.cancel)
+    private val noAction by composeTestRule.stringResource(R.string.action_none)
+    private val appActionType by composeTestRule.stringResource(ActionType.APP.nameStringId)
+    private val keystrokeActionType by composeTestRule.stringResource(ActionType.KEYSTROKE.nameStringId)
+    private val allActionTypes by lazy { setOf(appActionType, keystrokeActionType) }
+
+    @Test
+    fun cancelButton_onClick_dismisses() {
+        var dismissed = false
+        composeTestRule.setContent {
+            CreateActionSelectDialog(onDismiss = { dismissed = true })
+        }
+
+        composeTestRule.onNodeWithText(cancel).performClick()
+
+        Assert.assertTrue(dismissed)
+    }
+
+    // When all action types are available, all action types are displayed
+    @Test
+    fun allActionTypesAvailable_allActionTypesAreDisplayed() {
+        composeTestRule.setContent {
+            CreateActionSelectDialog(availableActionTypes = ActionType.entries.toSet())
+        }
+
+        for (actionType in allActionTypes) {
+            composeTestRule.onNodeWithText(actionType).assertIsDisplayed()
+        }
+    }
+
+    // When only one action type is available, other action types don't exist
+    @Test
+    fun singleActionTypeAvailable_singleAppActionTypeIsDisplayed() {
+        composeTestRule.setContent {
+            CreateActionSelectDialog(availableActionTypes = setOf(ActionType.APP))
+        }
+
+        composeTestRule.onNodeWithText(appActionType).assertIsDisplayed()
+        composeTestRule.onNodeWithText(keystrokeActionType).assertDoesNotExist()
+    }
+
+    // When initial action is null, `No action` option must stay selected on action type change
+    @Test
+    fun initialActionIsNull_actionTypeChange_noActionSelected() {
+        composeTestRule.setContent {
+            CreateActionSelectDialog(
+                availableActionTypes = ActionType.entries.toSet(),
+                initialAction = null,
+            )
+        }
+
+        composeTestRule.onNodeWithText(noAction).assertIsSelected()
+        for (actionType in allActionTypes) {
+            composeTestRule.onNodeWithText(actionType).performClick()
+            composeTestRule.onNodeWithText(noAction).assertIsSelected()
+        }
+    }
+
+    // All actions exist for app action type
+    @Test
+    fun appActionType_allActions_areDisplayed() {
+        composeTestRule.setContent {
+            CreateActionSelectDialog(
+                availableActionTypes = setOf(ActionType.APP),
+            )
+        }
+
+        for (appAction in AppAction.entries) {
+            val name = composeTestRule.activity.getString(appAction.nameStringId)
+            composeTestRule.onNodeWithText(name).assertExists()
+        }
+    }
+
+    @Suppress("TestFunctionName")
+    @Composable
+    private fun CreateActionSelectDialog(
+        availableActionTypes: Set<ActionType> = ActionType.entries.toSet(),
+        initialAction: Action? = null,
+        keystrokes: List<KeystrokeInfoUIState> = emptyList(),
+        onConfirm: (Action?) -> Unit = {},
+        onDismiss: () -> Unit = {},
+    ) {
+        SoundRemoteTheme {
+            ActionSelectDialog(
+                availableActionTypes = availableActionTypes,
+                initialAction = initialAction,
+                keystrokes = keystrokes,
+                onConfirm = onConfirm,
+                onDismiss = onDismiss,
+            )
+        }
+    }
+}
