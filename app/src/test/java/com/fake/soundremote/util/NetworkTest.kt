6f8ea8b5fea7a2f815264f5badc509b1b03b32ca
ashipo <24320267+ashipo@users.noreply.github.com>
2024-03-09 01:06:18 +0300

    Add tests for ByteBuffer reading operations

--- app/src/test/java/com/fake/soundremote/util/NetworkTest.kt
+++ app/src/test/java/com/fake/soundremote/util/NetworkTest.kt
@@ -1,6 +1,8 @@
 package com.fake.soundremote.util
 
+import com.fake.soundremote.util.Net.uByte
 import com.fake.soundremote.util.Net.uInt
+import com.fake.soundremote.util.Net.uShort
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -9,6 +11,61 @@ import java.nio.ByteBuffer
 
 @DisplayName("Network utils")
 internal class NetworkTest {
+
+    @Nested
+    @DisplayName("ByteBuffer.uByte")
+    inner class GetUByte {
+        @Test
+        @DisplayName("reads correctly")
+        fun readsCorrectly() {
+            val expected: UByte = 0xFAu
+            val buf = ByteBuffer.allocate(8)
+            buf.put(expected.toByte())
+            buf.rewind()
+
+            val actual = buf.uByte
+
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        @DisplayName("increments the ByteBuffer position by 1")
+        fun incrementsPositionCorrectly() {
+            val buf = ByteBuffer.allocate(8)
+
+            buf.uByte
+
+            assertEquals(1, buf.position())
+        }
+    }
+
+    @Nested
+    @DisplayName("ByteBuffer.uShort")
+    inner class GetUShort {
+
+        @Test
+        @DisplayName("reads correctly")
+        fun readsCorrectly() {
+            val expected: UShort = 0xFA_01u
+            val buf = ByteBuffer.allocate(8)
+            buf.putShort(expected.toShort())
+            buf.rewind()
+
+            val actual = buf.uShort
+
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        @DisplayName("increments the ByteBuffer position by 2")
+        fun incrementsPositionCorrectly() {
+            val buf = ByteBuffer.allocate(8)
+
+            buf.uShort
+
+            assertEquals(2, buf.position())
+        }
+    }
 
     @Nested
     @DisplayName("ByteBuffer.uInt")
@@ -28,11 +85,9 @@ internal class NetworkTest {
         }
 
         @Test
-        @DisplayName("advances ByteBuffer position by 4")
-        fun advancesCorrectly() {
+        @DisplayName("increments the ByteBuffer position by 4")
+        fun incrementsPositionCorrectly() {
             val buf = ByteBuffer.allocate(8)
-            buf.putInt(1)
-            buf.rewind()
 
             buf.uInt
 
