fde3e6fa759df3efd72791eaf353049d6248bc57
ashipo <24320267+ashipo@users.noreply.github.com>
2024-03-20 00:42:38 +0300

    Create tests for OpusAudioDecoder.outBufferSize

--- app/src/androidTest/java/com/fake/soundremote/audio/decoder/OpusAudioDecoderTest.kt
+++ app/src/androidTest/java/com/fake/soundremote/audio/decoder/OpusAudioDecoderTest.kt
@@ -1,5 +1,7 @@
 package com.fake.soundremote.audio.decoder
 
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.experimental.runners.Enclosed
 import org.junit.runner.RunWith
@@ -41,6 +43,34 @@ internal class OpusAudioDecoderTest {
         @Test(expected = DecoderException::class)
         fun test() {
             OpusAudioDecoder(rate, channels)
+        }
+    }
+
+    @RunWith(Parameterized::class)
+    internal class OutBuferSize_CalculatedCorrectly(
+        private val rate: Int,
+        private val channels: Int,
+        private val duration: Int,
+        private val expected: Int,
+    ) {
+        companion object {
+            @JvmStatic
+            @Parameters(name = "{0} Hz, {1} channel(s), {2} frame duration")
+            fun data(): Collection<Array<Any>> {
+                return listOf(
+                    arrayOf(8_000, 1, 5_000, 80),
+                    arrayOf(24_000, 1, 20_000, 960),
+                    arrayOf(48_000, 2, 10_000, 1_920),
+                )
+            }
+        }
+
+        @Test
+        fun test() {
+            val decoder = OpusAudioDecoder(rate, channels, duration)
+            val actual = decoder.outBufferSize
+
+            assertThat(actual, equalTo(expected))
         }
     }
 }
