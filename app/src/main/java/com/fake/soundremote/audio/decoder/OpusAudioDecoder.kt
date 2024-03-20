bae8e42e0c41a8e00c338cd02feb9d240b6df9e9
ashipo <24320267+ashipo@users.noreply.github.com>
2024-03-20 16:42:37 +0300

    Refactor OpusAudioDecoder
    Added method to calculate frame size in bytes from frame size in samples

--- app/src/main/java/com/fake/soundremote/audio/decoder/OpusAudioDecoder.kt
+++ app/src/main/java/com/fake/soundremote/audio/decoder/OpusAudioDecoder.kt
@@ -11,7 +11,7 @@ import com.fake.soundremote.util.Audio.SAMPLE_SIZE
  * Creates new OpusAudioDecoder
  * @param sampleRate sample rate in Hz, must be 8/12/16/24/48 KHz
  * @param channels number of channels, must be 1 or 2
- * @param frameDuration frame duration in microseconds, must be a multiple of 2.5ms up to 60ms
+ * @param frameDuration frame duration in microseconds, must be a multiple of 2.5ms from 2.5ms to 60ms
  */
 class OpusAudioDecoder(
     private val sampleRate: Int = SAMPLE_RATE,
@@ -19,13 +19,16 @@ class OpusAudioDecoder(
     private val frameDuration: Int = FRAME_DURATION,
 ) {
     private val opus = Opus()
-    private var sampleSize = SAMPLE_SIZE
-    private val samplesPerPacket = (sampleRate.toLong() * frameDuration / 1_000_000).toInt()
+    private val sampleSize = SAMPLE_SIZE
 
-    /** Bytes per decoded packet */
-    val outBufferSize = samplesPerPacket * channels * sampleSize
+    /** Number of samples per channel in one frame */
+    private val frameSize = (sampleRate.toLong() * frameDuration / 1_000_000).toInt()
+
+    /** Number of bytes per decoded frame */
+    val outBufferSize = samplesToBytes(frameSize)
 
     init {
+        check(frameDuration in 2_500..60_000) { "Opus decoder frame duration must be from from 2.5 ms to 60 ms" }
         val initResult = opus.initDecoder(sampleRate, channels)
         if (initResult != OPUS_OK) {
             val errorString = opus.strerror(initResult)
@@ -39,11 +42,15 @@ class OpusAudioDecoder(
 
     fun decode(encodedData: ByteArray, outPcm: ByteArray): Int {
         val dataSize = encodedData.size
-        val samplesDecoded = opus.decode(encodedData, dataSize, outPcm, samplesPerPacket, 0)
+        val samplesDecoded = opus.decode(encodedData, dataSize, outPcm, frameSize, 0)
         if (samplesDecoded < 0) {
             val errorString = opus.strerror(samplesDecoded)
             throw DecoderException("Opus decode error: $errorString")
         }
-        return samplesDecoded * channels * sampleSize
+        return samplesToBytes(samplesDecoded)
+    }
+
+    private fun samplesToBytes(samples: Int): Int {
+        return samples * channels * sampleSize
     }
 }
