0f857bc2b1459d59c968b2b004224927999ae445
ashipo <24320267+ashipo@users.noreply.github.com>
2024-11-22 18:31:59 +0300

    Refactor hotkey deletion dialog

--- app/src/main/java/io/github/soundremote/ui/hotkeylist/HotkeyListScreen.kt
+++ app/src/main/java/io/github/soundremote/ui/hotkeylist/HotkeyListScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
@@ -170,7 +171,16 @@ private fun HotkeyList(
                     Text(stringResource(R.string.cancel))
                 }
             },
-            text = { Text(stringResource(R.string.hotkey_delete_confirmation).format(name)) }
+            title = {
+                Text(stringResource(R.string.hotkey_delete_confirmation))
+            },
+            text = {
+                Text(
+                    text = name,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2,
+                )
+            }
         )
     }
 }
